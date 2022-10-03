b630f86743f45896c0ddd3fd21f75aa2ff909035
nailshaykhraziev <shaykhrazievnail@gmail.com>
2022-10-03 16:24:44 +0300

    Update dependecies

--- app/src/main/java/com/itis/template/MainActivity.kt
+++ app/src/main/java/com/itis/template/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.itis.template
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.itis.template.databinding.ActivityMainBinding
+import android.graphics.Color as AndroidColor
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    private var score = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // from binding
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+        binding?.run {
+
+            savedInstanceState?.getInt(ARG_SCORE)?.let {
+                score = it
+                tvHelloMessage1.text = "score: $it"
+            }
+
+            tvHelloMessage.setOnClickListener { btn ->
+                score++
+                tvHelloMessage1.text = "score: $score"
+            }
+
+            image.setOnClickListener {
+                sendImplictIntent()
+            }
+        }
+    }
+
+    private fun sendImplictIntent() {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$+78005553535®")
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+
+        val chooserIntent = Intent.createChooser(
+            intent,
+            "HIHI I am murlock"
+        )
+
+        if (chooserIntent.resolveActivity(packageManager) != null) {
+            startActivity(chooserIntent)
+        }
+
+    }
+
+    companion object {
+
+        private const val ARG_SCORE = "arg_score"
     }
 }

7c99d62ebc53b97e15415526ced9794778f3351e
nailshaykhraziev <shaykhrazievnail@gmail.com>
2023-02-20 14:07:50 +0300

    Update gradle and fix detekt issues

--- app/src/main/java/com/itis/template/MainActivity.kt
+++ app/src/main/java/com/itis/template/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.itis.template.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
 
     private var binding: ActivityMainBinding? = null
